9859be5674310ff089441ee6c41041c15ba3ff74
sudurr <sudursugunushev@gmail.com>
2023-09-01 20:14:39 +0600

    summaryRanges was added

---
+++ summaryRanges.playground/Contents.swift
@@ -0,0 +1,36 @@
+func summaryRanges(of nums: [Int]) -> [String] {
+
+    var sortedNums = nums.sorted()
+
+    guard !sortedNums.isEmpty else {return []}
+
+    var result = [String]()
+    var start = sortedNums[0]
+    var end = sortedNums[0]
+
+    for i in 1..<sortedNums.count {
+        if sortedNums[i] == end + 1 {
+            end = sortedNums[i]
+        } else {
+            if start == end {
+                result.append("\(start)")
+            } else {
+                result.append("\(start) -> \(end)")
+            }
+            start = sortedNums[i]
+            end = sortedNums[i]
+        }
+    }
+
+    if start == end {
+        result.append("\(start)")
+    } else {
+        result.append("\(start) -> \(end)")
+    }
+
+    return result
+}
+
+let array = [2,3,1,6,7,8,11,22,24,23]
+
+print(summaryRanges(of: array))
